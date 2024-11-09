c85a8663cf4df01842768a1e391be06fb718804a
Konda Srikanth <konda2srikanth@gmail.com>
2024-11-09 12:44:01 +0530

    Auto Push

---
+++ frontend.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ "$ID" -ne 0]; then
+echo -e "\e[31m Script is expected  to be  excuted as root user or with sudo scriptname.sh \e[0m"
+echo -e "\t sudo bash $0"
+exit 1
+fi
+dnf install nginx -y  
+cp proxy.conf /etc/nginx/default.d/expense.conf
+systemctl enable nginx 
+rm -rf /usr/share/nginx/html/*  
+curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+pwd
+ls -ltr
+
+systemctl restart nginx 
